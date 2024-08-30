96a80e78f5341ebb7d74f9748adbdb6bbabae6fd
oid85 <oid85@mail.ru>
2024-08-30 19:10:46 +0700

    oid85-01-03 добавление компонент

---
+++ Oid85.FinMarket/Oid85.FinMarket.External/Settings/sql/create_table_items.sql
@@ -0,0 +1,8 @@
+CREATE TABLE items (
+    id          INTEGER PRIMARY KEY
+                        UNIQUE
+                        NOT NULL,
+    key      TEXT,
+    value        TEXT,
+    description TEXT
+)
