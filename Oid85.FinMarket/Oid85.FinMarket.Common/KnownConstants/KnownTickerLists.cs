f17aa28c07c32562563a8246a1d11836e665f0dd
oid85 <oid85@mail.ru>
2024-10-14 22:05:59 +0700

    добавил вывод сектора

--- Oid85.FinMarket/Oid85.FinMarket.Common/KnownConstants/KnownTickerLists.cs
+++ Oid85.FinMarket/Oid85.FinMarket.Common/KnownConstants/KnownTickerLists.cs
@@ -2,6 +2,7 @@
 {
     public class KnownTickerLists
     {
+        public static string AllStocks = "AllStocks";
         public static string MoexIndexStocks = "MoexIndexStocks";
         public static string PortfolioStocks = "PortfolioStocks";
         public static string WatchListStocks = "WatchListStocks";
