b8920c6ca93ed2fc6212c16fd7effa89194eb081
oid85 <oid85@mail.ru>
2025-02-23 01:19:58 +0700

    добавил миграцию

---
+++ Oid85.FinMarket/Oid85.FinMarket.DataAccess/Migrations/20250222181728_AddInstrumentNameColumnToMarketEventsTable.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Oid85.FinMarket.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInstrumentNameColumnToMarketEventsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "instrument_name",
+                schema: "storage",
+                table: "market_events",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "instrument_name",
+                schema: "storage",
+                table: "market_events");
+        }
+    }
+}
