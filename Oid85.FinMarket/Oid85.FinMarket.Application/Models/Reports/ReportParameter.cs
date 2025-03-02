1fae642581670fdc900d808a37362d948189efd7
oid85 <oid85@mail.ru>
2025-03-03 03:22:09 +0700

    Update ReportParameter.cs

--- Oid85.FinMarket/Oid85.FinMarket.Application/Models/Reports/ReportParameter.cs
+++ Oid85.FinMarket/Oid85.FinMarket.Application/Models/Reports/ReportParameter.cs
@@ -2,10 +2,9 @@ using Oid85.FinMarket.Common.KnownConstants;
 
 namespace Oid85.FinMarket.Application.Models.Reports;
 
-public class ReportParameter(
-    string type, string value, string colorRgb = KnownColors.White)
+public class ReportParameter(string type, string value, string color = KnownColors.White)
 {
     public string Type { get; set; } = type;
     public string Value { get; set; } = value;
-    public string ColorRgb { get; set; } = colorRgb;
+    public string Color { get; set; } = color;
 }
