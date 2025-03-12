0440d7a027e7c8cc2792b0f513b7f4ef5fde9b7a
oid85 <oid85@mail.ru>
2025-03-12 10:35:32 +0700

    Create DebugController.cs

---
+++ Oid85.FinMarket/Oid85.FinMarket.WebHost/Controller/DebugController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Oid85.FinMarket.Application.Interfaces.Services;
+using Oid85.FinMarket.Application.Models.Responses;
+using Oid85.FinMarket.WebHost.Controller.Base;
+
+namespace Oid85.FinMarket.WebHost.Controller;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DebugController(
+    IMarketEventService marketEventService) 
+    : FinMarketBaseController
+{
+    /// <summary>
+    /// Отправить сообщение в телеграм
+    /// </summary>
+    [HttpGet("debug")]
+    [ProducesResponseType(typeof(BaseResponse<bool>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(BaseResponse<bool>), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(BaseResponse<bool>), StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> Debug()
+    {
+        await marketEventService.CheckStrikeDayMarketEventAsync();
+        return Ok();
+    }
+}
