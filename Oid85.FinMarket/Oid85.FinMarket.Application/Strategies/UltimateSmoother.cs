ad49d81925413ac031e06d6ad4d0d18ebc30253d
oid85 <oid85@mail.ru>
2025-06-21 19:51:19 +0700

    Create UltimateSmoother.cs

---
+++ Oid85.FinMarket/Oid85.FinMarket.Application/Strategies/UltimateSmoother.cs
@@ -0,0 +1,53 @@
+﻿using Oid85.FinMarket.Application.Interfaces.Factories;
+using Oid85.FinMarket.Common.MathExtensions;
+using Oid85.FinMarket.Domain.Models.Algo;
+
+namespace Oid85.FinMarket.Application.Strategies
+{
+    public class UltimateSmoother(
+        IIndicatorFactory indicatorFactory) 
+        : Strategy
+    {
+        public override void Execute()
+        {
+            // Получаем параметры
+            int period = Parameters["Period"];
+            
+            // Расчет индикаторов
+            List<double> ultimateSmoother = indicatorFactory.UltimateSmoother(ClosePrices, period);
+
+            for (int i = StabilizationPeriod; i < Candles.Count - 1; i++)
+            {
+                // Правило входа
+                SignalLong = 
+                    ultimateSmoother[i - 2] > ultimateSmoother[i - 3] && 
+                    ultimateSmoother[i - 1] > ultimateSmoother[i - 2] &&
+                    ultimateSmoother[i] > ultimateSmoother[i - 1];
+
+                // Правило выхода
+                SignalCloseLong = 
+                    ultimateSmoother[i - 2] < ultimateSmoother[i - 3] &&
+                    ultimateSmoother[i - 1] < ultimateSmoother[i - 2] &&
+                    ultimateSmoother[i] < ultimateSmoother[i - 1];
+                
+                // Задаем цену для заявки
+                double orderPrice = Candles[i].Close;
+
+                // Расчет размера позиции
+                int positionSize = GetPositionSize(orderPrice);
+                
+                if (LastActivePosition == null)
+                {
+                    if (SignalLong)
+                        BuyAtPrice(positionSize, orderPrice, i + 1);
+                }
+                
+                else
+                {
+                    if (SignalCloseLong)
+                        SellAtPrice(positionSize, orderPrice, i + 1);
+                }
+            }
+        }
+    }
+}
